c4ae032c120467c1e51c2847c3c481661f8e2fba
Mitchell Gilmore <mgilm0re@bu.edu>
2023-04-18 14:58:17 -0400

    base autoencoder

---
+++ models/autoender.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+
+
+class Autoencoder(nn.Module):
+
+	def __init__(self, **kargs) -> None:
+
+		super().__init__()
+
+		self.encoder = Encoder(**kargs)
+		self.decoder = Decoder(**kargs)
+
+	def forward(self,data):
+		return self.decoder(
+			self.encoder(data)
+		)
+
+class Encoder(nn.Module):
+
+	def __init__(
+			self,
+			convs = None,
+			activation=nn.Sigmoid,
+			initialization=nn.init.xavier_uniform_
+		) -> None:
+
+		super().__init__()
+
+		if convs is None:
+			convs = [3] + 9*[20]
+
+		# assert len(convs) == 9
+
+		self.weights = nn.Sequential()
+
+		for in_s, out_s in zip(convs[:-1],convs[1:]):
+
+			# allocates conv layer
+			conv = nn.Conv2d(in_s, out_s, 3, padding=1)
+
+			# initializes the conv layer
+			initialization(conv.weight)
+
+			block = nn.Sequential(
+				conv,
+				nn.MaxPool2d(2),
+				activation()
+			)
+
+			self.weights.append(block)
+
+	def forward(self,data):
+		return self.weights(data)
+
+class Decoder(nn.Module):
+
+	def __init__(
+			self,
+			convs = None,
+			activation=nn.Sigmoid,
+			initialization=nn.init.xavier_uniform_
+		) -> None:
+
+		super().__init__()
+
+		if convs is None:
+			convs = 9*[20] + [3]
+
+		# assert len(convs) == 9
+
+		self.weights = nn.Sequential()
+
+		for in_s, out_s in zip(convs[:-1],convs[1:]):
+
+			# allocates conv layer
+			conv = nn.ConvTranspose2d(in_s, out_s, 3, padding=1, stride=2, output_padding=1)
+
+			# initializes the conv layer
+			initialization(conv.weight)
+
+			block = nn.Sequential(
+				conv,
+				activation()
+			)
+
+			self.weights.append(block)
+
+	def forward(self,data):
+		return self.weights(data)
