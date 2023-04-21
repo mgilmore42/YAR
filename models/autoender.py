ef1bacf4a027e98e0b0c7cf4eef12cb72af63de9
Mitchell Gilmore <mgilm0re@bu.edu>
2023-04-21 10:27:40 -0400

    snapshot

--- models/autoender.py
+++ models/autoender.py
@@ -28,11 +28,26 @@ class Encoder(nn.Module):
 		super().__init__()
 
 		if convs is None:
-			convs = [3, 10, 20, 30, 40, 50, 60, 70, 85, 100]
+			convs = [30, 50, 100, 175, 250, 325, 375, 425, 500]
 
-		assert len(convs) == 10
+		# assert len(convs) == 10
 
 		self.weights = nn.Sequential()
+
+		conv =nn.Conv2d(3,convs[0], 3, padding=1)
+
+		# initializes the conv layer
+		initialization(conv.weight)
+
+		self.weights.append(
+			nn.Sequential(
+				conv,
+				activation(),
+				ResidualBlock(convs[0],blocks=4),
+				nn.MaxPool2d(2),
+				activation()
+			)
+		)
 
 		for in_s, out_s in zip(convs[:-1],convs[1:]):
 
@@ -46,13 +61,7 @@ class Encoder(nn.Module):
 				conv,
 				nn.MaxPool2d(2),
 				activation(),
-				nn.BatchNorm2d(out_s),
-				ResidualConnection(
-					nn.Sequential(
-						ResidualBlock(out_s),
-						ResidualBlock(out_s)
-					)
-				)
+				nn.BatchNorm2d(out_s)
 			)
 
 			self.weights.append(block)
@@ -72,7 +81,7 @@ class Decoder(nn.Module):
 		super().__init__()
 
 		if convs is None:
-			convs = [3, 10, 20, 30, 40, 50, 60, 70, 85, 100][::-1]
+			convs = [30, 50, 100, 175, 250, 325, 375, 425, 500][::-1]
 
 		# assert len(convs) == 9
 
@@ -81,24 +90,37 @@ class Decoder(nn.Module):
 		for in_s, out_s in zip(convs[:-1],convs[1:]):
 
 			# allocates conv layer
-			conv = nn.ConvTranspose2d(in_s, out_s, 3, padding=1, stride=2, output_padding=1)
+			conv_up = nn.ConvTranspose2d(in_s, in_s, 3, padding=1, stride=2, output_padding=1)
+			conv    = nn.Conv2d(in_s, out_s, 3, padding=1)
 
 			# initializes the conv layer
+			initialization(conv_up.weight)
 			initialization(conv.weight)
 
 			block = nn.Sequential(
+				conv_up,
 				conv,
 				activation(),
-				nn.BatchNorm2d(out_s),
-				ResidualConnection(
-					nn.Sequential(
-						ResidualBlock(out_s),
-						ResidualBlock(out_s)
-					)
-				)
+				nn.BatchNorm2d(out_s)
 			)
 
 			self.weights.append(block)
+
+		conv_up = nn.ConvTranspose2d(convs[-1], convs[-1], 3, padding=1, stride=2, output_padding=1)
+		conv    = nn.Conv2d(convs[-1], 3, 3, padding=1)
+
+		# initializes the conv layer
+		initialization(conv.weight)
+
+		self.weights.append(
+			nn.Sequential(
+				conv_up,
+				ResidualBlock(convs[-1],blocks=4),
+				activation(),
+				conv,
+				activation()
+			)
+		)
 
 	def forward(self,data):
 		return self.weights(data)
